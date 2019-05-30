604b91cfc056505f4718796f7d770e827e53fbd0
Yvan Sraka <yvan@sraka.pw>
2019-05-30 12:31:01 +0200

    Parse the content of the string read into a `Vec<Color>` and display a nice error message in case of wrong input

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,11 @@ fn fancy_print_guess(guess: Vec<Color>) {
             Color::Yellow => print!("{}", Yellow.paint("Y"))
         }
     }
+    println!("");
 }
 
 fn main() {
-    let guess = vec![Color::Blue, Color::Red, Color::Green, Color::Red];
+    let _secret = vec![Color::Blue, Color::Red, Color::Green, Color::Red];
 
     println!("███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗██████╗ ");
     println!("████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗");
@@ -30,14 +31,47 @@ fn main() {
     println!("╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ");
     println!("");
     //println!("Secret combination: ");
-    //fancy_print_guess(guess);
+    //fancy_print_input(input);
 
     loop {
-        let mut guess = String::new();
+        let mut input = String::new();
 
-        std::io::stdin().read_line(&mut guess)
+        std::io::stdin().read_line(&mut input)
           .expect("Failed to read line");
-        
-        println!("{}", guess);
+
+        // Mind about the newline character.
+        if input.len() != (4 + 1) {
+            println!("Sorry I am hard programmed to understand input of size four, can you help me?");
+            continue;
+        }
+
+        let mut guess = vec![];
+        let len = input.len();
+        for character in input[..len - 1].chars() {
+            let pawn = match character {
+                'B' => Color::Blue,
+                'R' => Color::Red,
+                'Y' => Color::Yellow,
+                'G' => Color::Green,
+
+                // Exaustive pattern
+                _ => {
+                    println!("Sorry dear human, I can see only four color please type:");
+                    println!("- R for red");
+                    println!("- B for blue");
+                    println!("- Y for Yellow");
+                    println!("- G for Green");
+                    println!("Otherwise I will be confused as you are.");
+                    break;
+                }
+            };
+            guess.push(pawn);
+        }
+        // Encountered an error in parsing?
+        if guess.len() != 4 {
+            continue;
+        }
+
+        fancy_print_guess(guess);
     }
 }
