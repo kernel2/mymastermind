aad76a5be5e17391c2474463cb26e0adbd044a0a
Yvan Sraka <yvan@sraka.pw>
2019-05-30 11:38:15 +0200

    Take advantage of `ansi_term` crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use ansi_term::Colour::{Red, Blue, Green, Yellow};
+
 #[derive(Debug)]
 enum Color {
     Red,
@@ -9,15 +11,24 @@ enum Color {
 fn fancy_print_guess(guess: Vec<Color>) {
     for pawn in guess {
         match pawn {
-            Color::Red => print!("R"),
-            Color::Blue => print!("B"),
-            Color::Green => print!("G"),
-            Color::Yellow => print!("Y")
+            Color::Red => print!("{}", Red.paint("R")),
+            Color::Blue => print!("{}", Blue.paint("B")),
+            Color::Green => print!("{}", Green.paint("G")),
+            Color::Yellow => print!("{}", Yellow.paint("G"))
         }
     }
 }
 
 fn main() {
     let guess = vec![Color::Blue, Color::Red, Color::Green, Color::Red];
+
+    println!("███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗██████╗ ");
+    println!("████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗");
+    println!("██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║██║  ██║");
+    println!("██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██║  ██║");
+    println!("██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██████╔╝");
+    println!("╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ");
+    println!("");
+    println!("Secret combination: ");
     fancy_print_guess(guess);
 }
